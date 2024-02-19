7a19bd7b9f848baeeb315d93377c4376a3927cbc
Pravinvignesh P <pravinvignesh2502@gmail.com>
2024-02-19 21:47:00 +0530

    commit

--- src/ECommerce/HomePage.js
+++ src/ECommerce/HomePage.js
@@ -1,27 +1,49 @@
 import {Link} from 'react-router-dom';
 import AllProduct from './AllProduct';
+
 export default function HomePage()
 {
 
     return (
         <div id="HomePage">
+            {/* <div id="headerdiv"> */}
                <header>
-                  <input type="text" placeholder="search product" ></input>
-                  <span><button>search</button></span>
-               </header>
-               <nav>
-                   <button><Link to="/"  >Home</Link></button>
-                   <button><a  >Products</a></button>
-                   <button><a  >Contacts</a></button>
-                   <button className="cart" ><Link to="/Ecom">cart</Link></button>
-               </nav>
-               {/* <div>
-                    <AllProduct></AllProduct>
-               </div> */}
-                
+                  
+                  <input type="text" className='form-control' placeholder="Search product" ></input>
+                    <button class="btn">Search</button> 
+                    <a href="/login" style={{float:"right"} }className="login-anchor">Login</a>
+
+                </header>
+                {/* </div> */}
+                <div class="col-xl-6 col-lg-7">
+                    <nav class="header__menu">
+                        <ul>
+                            <li class="active"><a href="./index.html">Home</a></li>
+                            <li><a href="#">Women’s</a></li>
+                            <li><a href="#">Men’s</a></li>
+                            <li><a href="./shop.html">Shop</a></li>
+                            <li><a href="#">Pages</a>
+                                <ul class="dropdown">
+                                    <li><a href="./product-details.html">Product Details</a></li>
+                                    <li><a href="./shop-cart.html">Shop Cart</a></li>
+                                    <li><a href="./checkout.html">Checkout</a></li>
+                                    <li><a href="./blog-details.html">Blog Details</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="./blog.html">Blog</a></li>
+                            <li><a href="./contact.html">Contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
+
+             
+
                {/* <footer>
                   <p id="footer">copyrights&copy</p>
                </footer> */}
         </div>
+
+      
+
     )
 }
