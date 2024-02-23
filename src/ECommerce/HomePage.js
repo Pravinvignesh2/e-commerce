d11d4314473d30c5586bc41f47e4a0b949e56218
srisundar2024 <157309037+srisundar2024@users.noreply.github.com>
2024-02-23 11:37:12 +0530

    Added Webapp Template Code

--- src/ECommerce/HomePage.js
+++ src/ECommerce/HomePage.js
@@ -2,61 +2,62 @@ import { Link } from 'react-router-dom';
 import AllProduct from './AllProduct';
 import { useLocation } from 'react-router-dom';
 import {useNavigate, useParams} from 'react-router-dom';
-
+import Header from "./Header";
 
 export default function HomePage(props)
 {
-
     // const location = useLocation();
     // const param1 = new URLSearchParams(location.search).get('param1');
     // const param3 = new URLSearchParams(location.search).get('param3');
 
     return (
-        <div id="HomePage">
-            {/* <div id="headerdiv"> */}
-               <header>
-                    <Link to="/AllProduct"><p>AIMPP</p></Link>
+        <>
+            <Header></Header>
+        </>
+        
+        // <div id="HomePage">
+        //     {/* <div id="headerdiv"> */}
+        //        <header>
+        //             <Link to="/AllProduct"><p>AIMPP</p></Link>
                  
-                    <input type="text" className='form-control' placeholder="Search product" ></input>
-                    <button className="btn">Search</button> 
+        //             <input type="text" className='form-control' placeholder="Search product" ></input>
+        //             <button className="btn">Search</button> 
 
-                    <Link to="/Login" style={{float:"right"} }className="login-anchor">Login</Link>
-                    {/* <p>{props.name}</p> */}
+        //             <Link to="/Login" style={{float:"right"} }className="login-anchor">Login</Link>
+        //             {/* <p>{props.name}</p> */}
 
-                    {/* <Link to="/Login">login</Link> */}
-                    <Link to="/Ecom" className='cart'><i class="fa fa-shopping-cart fa-xl" aria-hidden="true"></i></Link>
+        //             {/* <Link to="/Login">login</Link> */}
+        //             <Link to="/Ecom" className='cart'><i class="fa fa-shopping-cart fa-xl" aria-hidden="true"></i></Link>
 
 
-                </header>
-                {/* </div> */}
-                <div class="col-xl-6 col-lg-7">
-                    <nav class="header__menu">
-                        <ul>
-                            <li class="active"><Link to="/AllProduct">Home</Link></li>
-                            <li><a href="#">Women’s</a></li>
-                            <li><a href="#">Men’s</a></li>
-                            <li><a href="#">Pages</a>
-                                <ul class="dropdown">
-                                    <li><a href="./product-details.html">Product Details</a></li>
-                                    {/* <li><a href="./shop-cart.html">Shop Cart</a></li> */}
-                                    <li><a href="./checkout.html">Checkout</a></li>
-                                    <li><a href="./blog-details.html">Blog Details</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="./blog.html">Blog</a></li>
-                            <li><a href="./contact.html">Contact</a></li>
-                        </ul>
-                    </nav>
-                </div>
+        //         </header>
+        //         {/* </div> */}
+        //         <div class="col-xl-6 col-lg-7">
+        //             <nav class="header__menu">
+        //                 <ul>
+        //                     <li class="active"><Link to="/AllProduct">Home</Link></li>
+        //                     <li><a href="#">Women’s</a></li>
+        //                     <li><a href="#">Men’s</a></li>
+        //                     <li><a href="#">Pages</a>
+        //                         <ul class="dropdown">
+        //                             <li><a href="./product-details.html">Product Details</a></li>
+        //                             {/* <li><a href="./shop-cart.html">Shop Cart</a></li> */}
+        //                             <li><a href="./checkout.html">Checkout</a></li>
+        //                             <li><a href="./blog-details.html">Blog Details</a></li>
+        //                         </ul>
+        //                     </li>
+        //                     <li><a href="./blog.html">Blog</a></li>
+        //                     <li><a href="./contact.html">Contact</a></li>
+        //                 </ul>
+        //             </nav>
+        //         </div>
 
              
 
-               {/* <footer>
-                  <p id="footer">copyrights&copy</p>
-               </footer> */}
-        </div>
-
-      
+        //        {/* <footer>
+        //           <p id="footer">copyrights&copy</p>
+        //        </footer> */}
+        // </div>
 
     )
 }
