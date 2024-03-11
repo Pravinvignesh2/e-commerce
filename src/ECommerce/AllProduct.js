9c87aae9291572282e0ca1c1524e2c1afbfa8a83
AravinthanN <159110239+AravinthanN@users.noreply.github.com>
2024-03-11 12:58:06 +0530

    login

--- src/ECommerce/AllProduct.js
+++ src/ECommerce/AllProduct.js
@@ -55,89 +55,7 @@ export default function AllProduct(props) {
         setFilteredProducts(filtered);
         setCurrentPage(1); // Reset current page when filters change
     };
-
-// useEffect(() => {
-//     console.log("AllProduct:", AllProduct);
-//     console.log("currentPageProducts:", currentPageProducts);
-// }, [AllProduct, currentPageProducts]);
-
-const paginate = page => setCurrentPage(page);
-
-return (
-    <>
-    <>
-       <HomePage ></HomePage>
-       
-    </>
-    { AllProduct.length>0 && currentPageProducts.length>0 ? (
-        <>
-    
-    <div id="paginateDiv">
-    <button   id="leftButton" onClick={()=> setCurrentPage(currentPage-1)}  disabled={currentPage === 1}> L </button>
-
-    <div id="Allproduct">
-       
-        {
-            currentPageProducts.map(
-                (i)=>{
-                 return (
-                 <>
-                    <Link to={`/Allproduct/${i.id}`}>
-                         <div class="col-md-6 col-lg-4 col-xl-3 border border-secondary" id="ProductElement">
-                                <div class="rounded position-relative fruite-item">
-                                    <div class="fruite-img image-container " id="imge1">
-                                        <img src={i.image} class="img-fluid w-100 rounded-top" id="ecomimg" alt=""/>
-                                    </div>
-                                    <div class="text-white bg-secondary px-3 py-1 rounded position-absolute" style={{top: "10px", left: "10px"}}>
-                                    {categories.includes(i.category) ? i.category : 'Other'}
-                                    </div>
-                                    <div class="p-4  border-top-0 rounded-bottom">
-                                        <h4 id="title">{i.title}</h4>
-                                        <div className="d-flex mb-4">
-                                                <StarRating rating={i.rating.rate} />
-                                                <span className="ms-2">{i.rating.count} reviews</span>
-                                            </div>
-                                        {/* <p>{i.description}</p> */}
-                                        <div class="d-flex justify-content-between flex-lg-wrap" id="price-bottom">
-                                            <p  class="price text-dark fs-5 fw-bold mb-0">${i.price}</p>
-                                            <a href="#" class="btn border border-secondary rounded-pill px-3 text-primary" ><i class="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            
-                    </Link>
-
-                
-                
-                         </>)
-                }
-            )
-        }
-    </div>
-
-    <button  id="rightButton" onClick={()=> setCurrentPage(currentPage+1)}  disabled={currentPage===Math.ceil(AllProduct.length / productsPerPage)}>R</button> 
-    </div>
-        
-    </>              
-    ):null
-    }
-
-    { AllProduct.length>0 && currentPageProducts.length>0 &&
      
-          
-          <div id="paginate-container">
-              
-            
-               
-               {   (Array.from({ length: Math.ceil(AllProduct.length / productsPerPage) }, (_, index) => (
-                  
-                     <div id="loopButtonDiv">  <button id="loopButton" onClick={() => paginate(index + 1)}>{index + 1}</button></div>
-                  
-            )))}
-
-
-    // Filter products by maximum price
     const filterByPrice = (maxPrice) => {
         const filtered = allProducts.filter(product => product.price <= maxPrice);
         setFilteredProducts(filtered);
@@ -214,4 +132,5 @@ return (
             <Footer />
         </>
     );
+    
 }
